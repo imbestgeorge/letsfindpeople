665a9813f2bfdce954536f4e43fbac0232c2b150
mestregeorge <georgeteixeiraaguiar@gmail.com>
2026-04-29 12:19:06 +0100

    Vite error try fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,5 @@
 /* global process */
 
-const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1];
-const pagesBase = process.env.GITHUB_ACTIONS && repositoryName
-  ? `/${repositoryName}/`
-  : "/";
-
 export default {
-  base: process.env.VITE_BASE_PATH || pagesBase,
+  base: process.env.VITE_BASE_PATH || "/",
 };
